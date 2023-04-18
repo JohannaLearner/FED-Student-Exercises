78be39e58cf4152a38f0db0cc9a3ce45364a8761
Joh <joanne_learner@outlook.com>
2023-04-18 22:10:42 +0100

    Adding a new file with an even function

---
+++ 3 - JS/Week 2/week2Exercise1.js
@@ -0,0 +1,9 @@
+//This function calculates whether a number is even or odd
+function myEven(p)
+{
+    if (p%2 === 0)
+    return "even";
+    else
+    return "odd";   
+}
+console.log(myEven(19));
